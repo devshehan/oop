0bac2beb0600da7f849b625b51f191c8bad7e313
devshehan <shehaan.avishka00@gmail.com>
2023-07-01 13:58:01 +0530

    Custom Constructor

---
+++ 2.constructor.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Employee{
+
+public:
+    string name;
+    string companyName;
+    int age;
+
+    //create custom constructor
+    Employee(string empName, string empCompany, int empAge){
+        name = empName;
+        companyName = empCompany;
+        age = empAge;
+    }
+
+    void introduceYourSelf(){
+        cout << "Hello my name is: " << name << " and I am " << age << " years old." << endl;
+    }
+};
+
+int main(){
+
+    // create a object
+    Employee emp1 = Employee("Shehaan","LSEG",23);
+    emp1.introduceYourSelf();
+
+    Employee emp2 = Employee("Anuradha","IFS", 24);
+    emp2.introduceYourSelf();
+
+    return 0;
+}
