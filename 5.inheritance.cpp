890bf193ca94f9b7330d4d501f2a2e3e3d3a92a7
devshehan <shehaan.avishka00@gmail.com>
2023-07-02 07:27:21 +0530

    inheritance child access parent functions

--- 5.inheritance.cpp
+++ 5.inheritance.cpp
@@ -23,7 +23,7 @@ public:
     }
 };
 
-class Developer:Employee{
+class Developer: public Employee{
 private:
     string favProgrammingLang;
 public: 
@@ -38,14 +38,35 @@ public:
 
 };
 
+class Doctor: public Employee{
+private:
+    int numberOfsurgeries;
+public: 
+    Doctor(string username, string companyname, int age, int numberOfSurge)
+        : Employee(username, companyname, age){
+            numberOfsurgeries = numberOfSurge;
+    }
+
+    void doctorRecord(){
+        cout << name << " have done " << numberOfsurgeries << " surgeries successfully." << endl;
+    }
+
+};
+
 int main(){
 
     Employee e1 = Employee("Shehan", "LSEG", 23);
-    e1.introduceYourSelf();
+    // e1.introduceYourSelf();
     
-
     Developer d1 = Developer("Anuradha", "IFS", 24, "JavaScript");
     d1.fixBugs();
+    d1.introduceYourSelf();
+
+    cout << endl;
+
+    Doctor doc1 = Doctor("Sahan", "Goverment", 24, 12);
+    doc1.doctorRecord();
+    doc1.introduceYourSelf();
 
 
     return 0;
